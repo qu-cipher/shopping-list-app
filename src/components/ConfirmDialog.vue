e1d7d60960ed3d75c85eeb482fdbab6a1a25a89b
QuantumCipherr <120099354+qu-cipher@users.noreply.github.com>
2024-09-15 22:52:30 +0330

    add

---
+++ src/components/ConfirmDialog.vue
@@ -0,0 +1,33 @@
+<script setup>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.js';
+import '@/assets/dialog.css'
+
+const emit = defineEmits(['confirm']);
+</script>
+
+<template>
+    <div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="confirmModalLabel">Confirmation</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                Are you sure you want to delete this item?
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Cancel</button>
+                <button 
+                  type="button" 
+                  class="btn btn-danger" 
+                  data-bs-dismiss="modal" 
+                  @click="$emit('confirm')">
+                  Confirm
+                </button>
+            </div>
+            </div>
+        </div>
+    </div>
+</template>
